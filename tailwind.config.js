d8f3d6c4e4c26a13e4128192ba69ec908b4e59e0
challenge-repository <julioandarestu80@gmail.com>
2024-09-02 00:19:58 +0700

    Ngabuburit - Travel App

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["indext.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        abu: "#F4F5F6",
+        ungu: "#6A5EE8",
+      },
+    },
+  },
+  plugins: [],
+};
